8accbfc3bdf8b839707d003c941b41e7fd47a7e5
oussama-chenait <godiabout57@gmail.com>
2026-07-02 14:16:53 +0100

    Add files via upload

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
